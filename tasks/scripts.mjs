df9fa97d162f362c8e4891964c5cf98927c4d1d5
ZhurmanJulia <zhurmanjulia@mail.ru>
2023-05-11 22:36:30 +1000

    Финальная проверка

--- tasks/scripts.mjs
+++ tasks/scripts.mjs
@@ -5,13 +5,13 @@ import { isDev } from './mode.mjs'
 
 export function compileScripts() {
   const isDevMode = isDev()
-	return gulp.src('source/js/*.{js,ts}')
-			.pipe(gulpEsbuild({
-					bundle: true,
-					target: 'es2015',
-					sourcemap: isDevMode && 'external',
-					format: 'esm',
-					minify: !isDevMode
-			}))
-			.pipe(gulp.dest('build/js'))
+  return gulp.src('source/js/*.{js,ts}')
+      .pipe(gulpEsbuild({
+          bundle: true,
+          target: 'es2015',
+          sourcemap: isDevMode && 'external',
+          format: 'esm',
+          minify: !isDevMode
+      }))
+      .pipe(gulp.dest('build/js'))
 }
