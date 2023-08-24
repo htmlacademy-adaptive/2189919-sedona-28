b769e3e4ffd3a7d94d775bc2c6fb77b92e506aad
ZhurmanJulia <zhurmanjulia@mail.ru>
2023-08-24 19:11:05 +1000

    Исправляет ошибки. Защита.

--- source/js/form.js
+++ source/js/form.js
@@ -1,25 +1,25 @@
-const form = document.querySelector('form');
-const formModal = form.querySelector('.form__modal');
-const formSubmitButton = form.querySelector('.form__button');
-const buttonClose = form.querySelector('.form__modal-button');
-const formOverlay = form.querySelector('.form__modal-overlay');
+const form = document.querySelector("form");
+const formModal = form.querySelector(".form__modal");
+const formSubmitButton = form.querySelector(".form__button");
+const buttonClose = form.querySelector(".form__modal-button");
+const formOverlay = form.querySelector(".form__modal-overlay");
 
-formSubmitButton.addEventListener('click', (e) => {
+formSubmitButton.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const title = formModal.querySelector('.form__modal-title');
-  const message = formModal.querySelector('.form__modal-message');
-  const inputs = document.querySelectorAll('input');
+  const title = formModal.querySelector(".form__modal-title");
+  const message = formModal.querySelector(".form__modal-message");
+  const inputs = document.querySelectorAll("input");
   const info = {
-    'error' : {
-      'title' : 'Что-то пошло не так!',
-      'message' : 'Проверьте поля, выделенные красным, скорее всего вы забыли их заполнить',
-      'buttonText' : 'Ок'
+    "error" : {
+      "title" : 'Что-то пошло не так!',
+      "message" : 'Проверьте поля, выделенные красным, скорее всего вы забыли их заполнить',
+      "buttonText" : 'Ок'
     },
-    'success' : {
-      'title' : 'Ваш отзыв отправлен!',
-      'message' : 'Спасибо за ваше участие, ваш отзыв уже поступил к нам. <br>В ближайшее время мы опубликуем его на сайте.',
-      'buttonText' : 'Закрыть окно'
+    "success" : {
+      "title" : "Ваш отзыв отправлен!",
+      "message" : "Спасибо за ваше участие, ваш отзыв уже поступил к нам. <br>В ближайшее время мы опубликуем его на сайте.",
+      "buttonText" : "Закрыть окно"
     }
   }
   const isValid = form. checkValidity ();
@@ -28,25 +28,25 @@ formSubmitButton.addEventListener('click', (e) => {
   title.innerHTML = text.title;
   message.innerHTML = text.message;
   buttonClose.innerHTML = text.buttonText;
-  formModal.classList.toggle('form__modal--success', isValid);
+  formModal.classList.toggle("form__modal--success", isValid);
 
-  formOverlay.classList.add('form__modal-overlay--opened');
-  formModal.classList.add('form__modal--opened');
+  formOverlay.classList.add("form__modal-overlay--opened");
+  formModal.classList.add("form__modal--opened");
 });
 
 const modalClose = () => {
-  formModal.classList.remove('form__modal--opened');
-  formModal.classList.remove('form__modal--success');
-  formOverlay.classList.remove('form__modal-overlay--opened');
+  formModal.classList.remove("form__modal--opened");
+  formModal.classList.remove("form__modal--success");
+  formOverlay.classList.remove("form__modal-overlay--opened");
 }
 
 /* закрытие окон */
-formOverlay.addEventListener('click', modalClose);
-buttonClose.addEventListener('click', modalClose);
+formOverlay.addEventListener("click", modalClose);
+buttonClose.addEventListener("click", modalClose);
 
 /* закрытие по ESC */
-document.body.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape' || e.key === 'Esc') {
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" || e.key === "Esc") {
     modalClose();
   };
 }, false);
--- source/js/index.js
+++ source/js/index.js
@@ -1,15 +1,17 @@
 
-let navMain = document.querySelector('.main-navigation');
-let navToggle = document.querySelector('.main-navigation__button');
+let navMain = document.querySelector(".main-navigation");
+let navToggle = document.querySelector(".main-navigation__button");
 
-navMain.classList.remove('main-navigation--no-js');
+navMain.classList.remove("main-navigation--no-js");
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-navigation--closed')) {
-    navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-navigation--closed")) {
+    navMain.classList.remove("main-navigation--closed");
+    navMain.classList.add("main-navigation--opened");
   } else {
-    navMain.classList.add('main-navigation--closed');
-    navMain.classList.remove('main-navigation--opened');
+    navMain.classList.add("main-navigation--closed");
+    navMain.classList.remove("main-navigation--opened");
   }
 });
+
+
